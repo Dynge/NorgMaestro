59c3d8d183605f1735221402d9705c8d4eb087b3
Michael Damsbo Lyngs <michael_lyngs@outlook.dk>
2024-07-10 13:23:22 +0200

    feat: can search on workspace symbols ✨

--- NorgMaestro.Server/Methods/WorkspaceSymbolHandler.cs
+++ NorgMaestro.Server/Methods/WorkspaceSymbolHandler.cs
@@ -1,34 +1,60 @@
+using System.Text.RegularExpressions;
 using NorgMaestro.Rpc;
 
 namespace NorgMaestro.Methods
 {
-    public class WorkspaceSymbolHandler : IMessageHandler
+    public partial class WorkspaceSymbolHandler : IMessageHandler
     {
         public required RpcMessage Request { get; init; }
         public required LanguageServerState State { get; init; }
+
+        [GeneratedRegex(@"^\[(\w+)\]")]
+        private static partial Regex KindRegex();
 
         public Response HandleRequest()
         {
             WorkspaceSymbolRequest workspaceRequest = WorkspaceSymbolRequest.From(Request);
             List<WorkspaceSymbol> symbols = [];
+            Func<WorkspaceSymbol, bool> filter = workspaceRequest.Params.Query switch
+            {
+                string q when KindRegex().IsMatch(q)
+                    => (WorkspaceSymbol wsSymbol) =>
+                    {
+                        var symbolMatch = KindRegex().Match(q).Groups[1].Value;
+                        if (Enum.TryParse<SymbolKind>(symbolMatch, out var kind) is false)
+                        {
+                            return false;
+                        }
+                        return wsSymbol.Kind == kind;
+                    },
+                string q
+                    => (WorkspaceSymbol wsSymbol) =>
+                    {
+                        return wsSymbol.Name.Contains(q);
+                    },
+            };
             foreach (Document doc in State.Documents.Values)
             {
-                symbols.Add(
-                    new()
+                var symbol = new WorkspaceSymbol()
+                {
+                    Location = new()
                     {
-                        Location = new()
+                        Range = new()
                         {
-                            Range = new()
-                            {
-                                Start = new() { Line = 0, Character = 0 },
-                                End = new() { Line = 0, Character = 0 }
-                            },
-                            Uri = doc.Uri.AbsoluteUri,
+                            Start = new() { Line = 0, Character = 0 },
+                            End = new() { Line = 0, Character = 0 }
                         },
-                        Kind = SymbolKind.File,
-                        Name = doc.Metadata.Title?.Name ?? "",
-                    }
-                );
+                        Uri = doc.Uri.AbsoluteUri,
+                    },
+                    Kind = SymbolKind.File,
+                    Name = doc.Metadata.Title?.Name ?? "",
+                };
+
+                if (filter(symbol) is false)
+                {
+                    continue;
+                }
+                symbols.Add(symbol);
             }
             return Response.OfSuccess(workspaceRequest.Id, symbols.ToArray());
         }
